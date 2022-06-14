eaa104a1b2ddc759398d4fd5e88a94e0edcbcd99
erickkartiadi <erickcartiady@gmail.com>
2022-06-14 12:09:42 +0700

    :sparkles: dynamic transaction page content

--- js/transaction-page.js
+++ js/transaction-page.js
@@ -1,9 +1,31 @@
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const src = urlParams.get("src");
-console.log(src);
+window.addEventListener("load", changeTransactionPageContent());
 
-if (src) {
-  let transactionNFTImage = document.getElementById("transactionNFTImage");
-  transactionNFTImage.setAttribute("src", src);
+function changeTransactionPageContent() {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const src = urlParams.get("src");
+  const nftName = urlParams.get("nftName");
+  const currentBid = urlParams.get("currentBid");
+
+  if (src) {
+    let transactionNFTImage = document.getElementById("transactionNFTImage");
+    transactionNFTImage.setAttribute("src", src);
+  }
+
+  if (nftName) {
+    let transatctionNFTName = document.getElementById("transatctionNFTName");
+    transatctionNFTName.textContent = nftName;
+  }
+
+  if (nftName) {
+    let transatctionNFTName = document.getElementById("transatctionNFTName");
+    transatctionNFTName.textContent = nftName;
+  }
+
+  if (currentBid) {
+    let transactionCurrentBid = document.getElementById(
+      "transactionCurrentBid"
+    );
+    transactionCurrentBid.textContent = currentBid + " ETH";
+  }
 }
